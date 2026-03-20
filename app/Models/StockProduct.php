8a93ac36bc8fc6f961318fcd3a5013ec9e93aae6
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-20 09:18:52 +0700

    Introduction POS App | ORM StockProduct

--- app/Models/StockProduct.php
+++ app/Models/StockProduct.php
@@ -10,4 +10,21 @@ class StockProduct extends Model
     use HasFactory;
     
     protected $guarded = [];
+
+    /**
+     * Relasi ke model Product.
+     * Satu stock product hanya terkait dengan satu produk
+     */
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * Relasi ke model Supplier.
+     * Satu stock product hanya terkait dengan satu supplier.
+     */
+
+    public function supplier() {
+        return $this->belongsTo(Supplier::class);
+    }
 }
