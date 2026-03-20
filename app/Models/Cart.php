b94d19e09987f4987fd3f0f4aa320b8c945c0406
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-20 10:00:11 +0700

    Introduction POS App | ORM Cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
-    
     protected $guarded = [];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    } 
 }
