48fbb192047bef48bffb61eb23649b3a09adf8b6
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-20 09:11:35 +0700

    Introduction POS App | ORM Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,32 @@ class Product extends Model
     use HasFactory; 
     
     protected $guarded = [];
+
+    /**
+     * Relasi ke model Category
+     * Satu produk hanya memiliki satu kategori
+     */
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+    
+    /**
+     * Relasi ke model StockTotal
+     * Satu produk memiliki satu total stok
+     */
+    public function stockTotal() {
+        return $this->hasOne(StockTotal::class);
+    }
+    
+    /**
+     * Aksesors untuk mendapatkan URL gambar produk 
+     */
+    public function image() {
+        return Attribute::make(
+            get: fn($image) => url('/storage/products'. $image),
+        );
+    }
+
+
+
 }
