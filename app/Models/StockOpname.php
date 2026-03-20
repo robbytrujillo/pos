2b2d7742b266271c03bc3b44d9cfae801284dca0
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-20 10:02:48 +0700

    Introduction POS App | ORM StockOpname

--- app/Models/StockOpname.php
+++ app/Models/StockOpname.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class StockOpname extends Model
 {
     use HasFactory;
-    
+
     protected $guarded = [];
+
+    /**
+     * Relasi ke model StockOpnameDetail.
+     * Satu stock opname memiliki banyak detail stock opname.
+     */
+    public function details() {
+        return $this->hasMany(StockOpnameDetail::class);
+    }
 }
